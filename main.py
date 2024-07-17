750d244097bfd776d7820b12fbf3ea69d28d6a4d
gokhaneraslan <gokhaneraslan@outlook.com>
2024-07-17 18:01:49 +0300

    update main.py

--- main.py
+++ main.py
@@ -62,4 +62,4 @@ async def post_document(query: Query):
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, log_level="info")
+    uvicorn.run("main:app", host="0.0.0.0", port=8000)
